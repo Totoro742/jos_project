bf79283c4708afbe88c5108ecf28ed5c2dff0ff6
Jan Bizon <janek.bizon@gmail.com>
2023-05-26 08:40:14 +0200

    oper

--- jos_project.srcs/sources_1/new/fsm/fsm_oper.sv
+++ jos_project.srcs/sources_1/new/fsm/fsm_oper.sv
@@ -7,8 +7,17 @@ typedef enum {Op_Idle, Op_Screen, Op_Page, Op_PageInit, Op_SendChar,
 module fsm_oper(input clk, input rst, input en, output fin, output logic dc);
     string text_to_send = "abcd";
 
-    logic [7:0] characters [1:5];
+    string char = text_to_send[0];
+
+    logic [15:0] characters [1:95] [1:16];
     initial $readmemh("char2pixels.mem", characters);
+
+    logic [15:0] char_bit [1:16];
+    logic [15:0] column;
+    logic [7:0] p1;
+    logic [7:0] p2;
+
+    // offset calculation
 
     localparam delay_ms = 500;
     localparam n = $clog2(delay_ms); // zle nie o to chodzi
@@ -24,15 +33,17 @@ module fsm_oper(input clk, input rst, input en, output fin, output logic dc);
     reg [bits-1:0] sh_reg;
     logic [2:0] cnt_spi;
     logic [7:0] leds;
+    logic spi_en;
 
-    spi #(.bits(bits)) master_oled (.clk(clk), .rst(rst), .en(spi_en), .miso(), .clr_ctrl(clr_ctrl), .data2trans(),
+    spi #(.bits(bits)) master_oled (.clk(clk), .rst(rst), .en(spi_en),
+     .miso(), .clr_ctrl(), .data2trans(data2trans),
     .clr(clr), .ss(s), .sclk(sclk), .mosi(mosi), .data_rec(data_rec), .fin(spi_fin));
 
     logic up_dc;
 
     update_page pager(.clk(clk), .rst(pager_rst), .en(pager_en), .fin(page_fin), .dc(up_dc));
 
-
+ // delay
 
     always @(posedge clk) begin
         dc <= up_dc;
@@ -61,27 +72,42 @@ module fsm_oper(input clk, input rst, input en, output fin, output logic dc);
                 end 
             end
             Op_PageInit: begin // page var init ???
-                if (cnt_page == 8) cnt_page = 3'b0;
+                if (cnt_page == 8) begin
+                    cnt_page = 3'b0;
+                    char_cnt = 0;
+                end
                 next_state = Op_Page;
             end
             Op_SendChar: begin // ???
-                 next_state = Op_ReadMem;
+                char_bit = characters[`int(char)-31]
+                column = char_bit[1];
+                p1 = column[7:0];
+                p2 = column[15:8];
+                char_cnt <= char_cnt + 1;
+                next_state = Op_ReadMem;
             end
             Op_ReadMem: begin // read char from memory???
                  next_state = Op_Spi1;
             end
             Op_Spi1: begin // comm ??
-                spi_en_r = 1'b1;
-                 if(spi_fin) begin
-                    spi_en_r = 1'b0;
+                data2trans = data; // address
+                spi_en = 1'b1;
+                num_cnt = 0;
+                if(spi_fin) begin
+                    spi_en = 1'b0;
                     next_state = Op_Spi2;
                  end
             end
             Op_Spi2: begin // data ???
-                spi_en_r = 1'b1;
-                 if(spi_fin) begin
-                     spi_en_r = 1'b0;
-                     next_state = Op_Back;
+                data2trans = data; // data
+                spi_en = 1'b1;
+                if(num_cnt == 16) begin
+                    spi_en = 1'b0;
+                    next_state = Op_Back;
+                end
+                else if(spi_fin) begin
+                    clm_cnt <= clm_cnt + 1;
+                    num_cnt <= num_cnt + 1;
                  end
             end
             //Back: next_state = SendChar, PageInit, TimeDisp; // decision: 1. next page, 2. delay - screen finished, 3. draw chars on same page ???
@@ -103,4 +129,5 @@ module fsm_oper(input clk, input rst, input en, output fin, output logic dc);
         else
             curr_state <= next_state;     
     end
+    
 endmodule
